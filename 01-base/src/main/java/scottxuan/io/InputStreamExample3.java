fbab43d8f08ff6f8370138419aa0be9b7c106cc2
scottxuan <zhaoxuan734@163.com>
2020-08-25 23:30:12 +0800

    InPutStream

---
+++ 01-base/src/main/java/scottxuan/io/InputStreamExample3.java
@@ -0,0 +1,24 @@
+package scottxuan.io;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.util.Properties;
+
+@Slf4j
+public class InputStreamExample3 {
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        try(InputStream inputStream = InputStreamExample3.class.getResourceAsStream("/111.png")){
+            try(OutputStream outputStream = new FileOutputStream("C:\\Users\\scottxuan\\Desktop\\222.png")){
+                byte[] bytes = new byte[1024];
+                int n;
+                while ((n = inputStream.read(bytes)) != -1) {
+                    outputStream.write(bytes,0,n);
+                }
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        log.info("耗时:{}毫秒",endTime-startTime);
+    }
+}
